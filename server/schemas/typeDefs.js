010c3a84248492942a37f3b77c45c88ab44b74bf
Steven Ly <ly.steven16@gmail.com>
2021-07-27 04:52:59 -0500

    Begin working on queries and mutations. Type Defs and Resolvers done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,18 +4,44 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
 
+
+type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
 type Book {
-    _id: ID
-    authors: String
+    _id: ID!
+    bookId: String
+    authors: [String]
     description: String
+    image: String
+    link: String
+}
+
+type Query {
+    me: User
+  }
+
+input savedBook {
+    authors: [String]
+    description: String
+    title: String
     bookId: String
     image: String
     link: String
-    title: String
-  }
-type Query {
-    books: [Book]
-  }
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook): User
+    removeBook(bookId: String): User
+}
+
 type Auth {
     token: ID!
     user: User
